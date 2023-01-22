bb16b4893a97c31e36297829b9680322c7c6d1cd
Doskulov Mirbek <iskender.doskulov@gmail.com>
2023-01-22 14:58:13 +0600

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Header = () => {
+	return (
+		<>
+			<header className='bg-900'>
+				<div className=''>
+					<h1>Logo</h1>
+				</div>
+				<div>
+					<ul>
+						<li>РЕЙТИНГ</li>
+						<li>БОЙЦЫ</li>
+						<li>ТУРНИРЫ</li>
+						<li>НОВОСТИ</li>
+					</ul>
+				</div>
+			</header>
+		</>
+	)
+}
+
+export default Header
