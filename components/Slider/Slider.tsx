3acd5393eba4fcb9b37fe52e5c34f43f1c2f15fc
Doskulov Mirbek <iskender.doskulov@gmail.com>
2023-01-23 22:52:40 +0600

    Add Carousel

--- components/Slider/Slider.tsx
+++ components/Slider/Slider.tsx
@@ -1,14 +1,36 @@
 import Image from 'next/image'
+import { Carousel } from 'react-responsive-carousel'
 import styles from '../Slider/Slider.module.scss'
 
 const Slider = () => {
 	return (
 		<div>
-			<img
-				className={styles.slider}
-				src='https://i.ibb.co/FK2gWP2/ufc270.jpg'
-				alt=''
-			/>
+			<Carousel>
+				<div>
+					<img
+						className={styles.slider}
+						src='https://i.ibb.co/FK2gWP2/ufc270.jpg'
+						alt=''
+					/>
+					<p className='legend'>Legend 1</p>
+				</div>
+				<div>
+					<img
+						className={styles.slider}
+						src='https://i.ibb.co/FK2gWP2/ufc270.jpg'
+						alt=''
+					/>
+					<p className='legend'>Legend 2</p>
+				</div>
+				<div>
+					<img
+						className={styles.slider}
+						src='https://i.ibb.co/FK2gWP2/ufc270.jpg'
+						alt=''
+					/>
+					<p className='legend'>Legend 3</p>
+				</div>
+			</Carousel>
 		</div>
 	)
 }
